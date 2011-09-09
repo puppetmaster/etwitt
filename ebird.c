1883627b44c67c2e7355356d30ed30c6a862bef0
Philippe 'Puppet_Master' Caseiro <caseiro.philippe@gmail.com>
2011-09-09 21:40:45 +0200

    Improving error management and authenticity key retreive

--- ebird.c
+++ ebird.c
@@ -9,10 +9,10 @@
 #define EBIRD_ACCESS_TOKEN_URL "https://api.twitter.com/oauth/access_token"
 
 
-#define EBIRD_USER_SCREEN_NAME "xxxxxxxx"
+#define EBIRD_USER_SCREEN_NAME "xxxxxxxxxxxxxx"
 #define EBIRD_USER_EMAIL "xxxxe@xxxx.com"
 #define EBIRD_USER_ID "xxxxxxxxx"
-#define EBIRD_USER_PASSWD "x-x-x-x-x"	//<< percent encode: char "+" => %2B
+#define EBIRD_USER_PASSWD "xxxxxxxx"	//<< percent encode: char "+" => %2B
 #define EBIRD_USER_CONSUMER_KEY "xxxxxxxxxxxxxxxxxxxxx"
 #define EBIRD_USER_CONSUMER_SECRET "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 #define EBIRD_USER_ACCESS_TOKEN_KEY "xxxxx"
@@ -42,21 +42,34 @@ ebird_request_token_get(OauthToken *request)
 	int res;
 	int i;
 
+	printf("ici\n");
 	request->url = oauth_sign_url2(EBIRD_REQUEST_TOKEN_URL, NULL, OA_HMAC, NULL, 
                                    EBIRD_USER_CONSUMER_KEY,                     
                                    EBIRD_USER_CONSUMER_SECRET, NULL, NULL);
-	request->token = oauth_http_get(request->url,NULL);
-	res = oauth_split_url_parameters(request->token,&request->token_prm);
-
-	if (res = 3)
+	if (request->url)
 	{
-		request->key = strdup(&(request->token_prm[0][12]));
-		request->secret = strdup(&(request->token_prm[1][19]));
+		request->token = oauth_http_get(request->url,NULL);
+		if (strcmp(request->token,"Failed to validate oauth signature and token"))
+			printf("Error");
+		else
+		{
+			printf("DEBUG ICI%s\n",request->token);
+			res = oauth_split_url_parameters(request->token,&request->token_prm);
+
+			if (res = 3)
+			{
+				request->key = strdup(&(request->token_prm[0][12]));
+				request->secret = strdup(&(request->token_prm[1][19]));
+			}
+			else
+			{
+				printf("Error on Request Token [%s]",request->token);
+			}
+		}
+
 	}
 	else
-	{
-		printf("Error on Request Token [%s]",request->token);
-	}
+		printf("Error\n");
 
 }
 
@@ -64,9 +77,22 @@ char *
 ebird_authenticity_token_get(char *web_script)
 {
     char *result;
+	char *keyword;
+	char *page;
+	char *key;
 
-    result = strstr(web_script,"twttr.form_authenticity_token");
-    return result;
+	keyword = strdup("twttr.form_authenticity_token");
+
+    page = strstr(web_script,keyword);
+	if (page)
+	{
+		result = strtok(page,"'");
+		key = strtok(NULL,"'");
+		return key;
+	}
+	else
+		return NULL;
+
 }
 
 static void
@@ -83,9 +109,6 @@ ebird_direct_token_get(char *key)
    url = strdup(buf);
    script = oauth_http_get(url, NULL);
    authenticity_token = ebird_authenticity_token_get(script);
-   printf("=============================================================================\n");
-   printf("%s\n",authenticity_token);
-   printf("=============================================================================\n");
    free(url);
 }
 
